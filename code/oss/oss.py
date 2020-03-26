846c4021bc62fb56dd9bfa4e2f0ee8b0fa44e1fc
hatlonely <hatlonely@gmail.com>
2020-03-26 23:43:51 +0800

    update

--- code/oss/oss.py
+++ code/oss/oss.py
@@ -3,35 +3,39 @@
 # pip3 install oss2
 # pip3 install aliyun-python-sdk-imm
 
-import oss2
-import configparser
 import os
+import oss2
+import util
 
-access_key_id = "see https://usercenter.console.aliyun.com/#/manage/ak"
-access_key_secret = "see https://usercenter.console.aliyun.com/#/manage/ak"
-endpoint="oss-cn-beijing.aliyuncs.com"
+config = util.load_credential_from_local()
+access_key_id = config["accessKeyID"]
+access_key_secret = config["accessKeySecret"]
+region_id = config["regionID"]
+region = config["region"]
+endpoint = config["endpoint"]
 
-def env():
-    global access_key_id
-    global access_key_secret
-    global endpoint
-    config = configparser.ConfigParser()
-    config.read(os.path.join(os.path.expanduser("~"), ".ossutilconfig"))
-    access_key_id = config["Credentials"]["accessKeyID"]
-    access_key_secret = config["Credentials"]["accessKeySecret"]
-    endpoint = config["Credentials"]["endpoint"]
 
-def main():
-    env()
-    auth = oss2.Auth(access_key_id, access_key_secret)
-    bucket = oss2.Bucket(auth, endpoint, 'hatlonely-test-bucket')
-    
-    key = "hello.txt"
-    bucket.put_object(key, 'Ali Baba is a happy youth.')
-    bucket.get_object(key).read()
+auth = oss2.Auth(access_key_id, access_key_secret)
+bucket = oss2.Bucket(auth, endpoint, 'hatlonely-test-bucket')
 
+
+def put_object(filename):
+    bucket.put_object(os.path.basename(filename), open(filename, "rb").read())
+
+
+def del_object(filename):
+    bucket.delete_object(filename)
+
+
+def list_object():
     for object_info in oss2.ObjectIterator(bucket):
         print(object_info.key)
+
+
+def main():
+    put_object("../asset/test.docx")
+    del_object("test.docx")
+    list_object()
 
 
 if __name__ == '__main__':
