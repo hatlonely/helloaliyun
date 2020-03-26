2715ce1cae91924acfe4398bd38efd1c9a120c21
hatlonely <hatlonely@gmail.com>
2020-03-27 00:20:36 +0800

    update

--- code/imm/doc.py
+++ code/imm/doc.py
@@ -5,43 +5,47 @@ import datetime
 import random
 import util
 import urllib
+import oss2
+import os
 
 config = util.load_credential_from_local()
 access_key_id = config["accessKeyID"]
 access_key_secret = config["accessKeySecret"]
 region_id = config["regionID"]
 region = config["region"]
+endpoint = config["endpoint"]
+bucket = 'hatlonely-test-bucket'
 
 
-def preview():
-    # https://help.aliyun.com/document_detail/74947.html?spm=a2c4g.11186623.2.22.335226c5dUnarR
-    res = requests.get("https://preview.imm.aliyuncs.com/index.html", params={
-        "url": "https://hatlonely-test-bucket.{}.aliyuncs.com/hello.docx/imm".format(region),
-        "accessKeyId": access_key_id,
-        "accessKeySecret": access_key_secret,
-        "region": region,
-        "bucket": "hatlonely-test-bucket"
-    })
-    print(urllib.parse.unquote(res.url))
+def put_object(filename):
+    auth = oss2.Auth(access_key_id, access_key_secret)
+    b = oss2.Bucket(auth, endpoint, bucket)
+    b.put_object(os.path.basename(filename), open(filename, "rb").read())
 
 
-def convert():
+def del_object(filename):
+    auth = oss2.Auth(access_key_id, access_key_secret)
+    b = oss2.Bucket(auth, endpoint, bucket)
+    b.delete_object(filename)
+
+
+def convert(filename, type):
     random.seed()
     params = {
         "Project": "ossdocdefault",
         "Format": "JSON",
         "AccessKeyId": access_key_id,
         "RegionId": region_id,
-        "Bucket": "hatlonely-test-bucket",
+        "Bucket": bucket,
         "Version": "2017-09-06",
         "Timestamp": datetime.datetime.utcnow().isoformat(),
         "SignatureMethod": "HMAC-SHA1",
         "SignatureVersion": "1.0",
         "SignatureNonce": random.randint(0, 2**63-1),
         "Action": "ConvertOfficeFormat",
-        "SrcUri": "oss://hatlonely-test-bucket/hello.docx",
-        "TgtType": "vector",
-        "TgtUri": "oss://hatlonely-test-bucket/hello.docx/imm"
+        "SrcUri": "oss://{}/{}".format(bucket, filename),
+        "TgtType": type,  # vector | jpg | png | pdf | text
+        "TgtUri": "oss://{}/{}/imm/{}".format(bucket, filename, type)
     }
     params["Signature"] = util.signature("POST", params, access_key_secret)
 
@@ -51,9 +55,26 @@ def convert():
     print(res.content)
 
 
+def preview(filename):
+    # https://help.aliyun.com/document_detail/74947.html?spm=a2c4g.11186623.2.22.335226c5dUnarR
+    res = requests.get("https://preview.imm.aliyuncs.com/index.html", params={
+        "url": "https://{}.{}.aliyuncs.com/{}/imm/vector".format(bucket, region, filename),
+        "accessKeyId": access_key_id,
+        "accessKeySecret": access_key_secret,
+        "region": region,
+        "bucket": bucket
+    })
+    print(urllib.parse.unquote(res.url))
+
+
 def main():
-    convert()
-    preview()
+    put_object("../asset/test.docx")
+    convert("test.docx", "png")
+    convert("test.docx", "jpg")
+    convert("test.docx", "text")
+    convert("test.docx", "pdf")
+    convert("test.docx", "vector")
+    preview("test.docx")
 
 
 if __name__ == "__main__":
---
+++ code/oss/util.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import os
+import configparser
+import urllib.parse
+import hmac
+import base64
+import unittest
+
+region_to_endpoint = {
+    "oss-cn-hangzhou": "oss-cn-hangzhou.aliyuncs.com",
+    "oss-cn-shanghai": "oss-cn-shanghai.aliyuncs.com",
+    "oss-cn-qingdao": "oss-cn-qingdao.aliyuncs.com",
+    "oss-cn-beijing": "oss-cn-beijing.aliyuncs.com",
+    "oss-cn-zhangjiakou": "oss-cn-zhangjiakou.aliyuncs.com",
+    "oss-cn-huhehaote": "oss-cn-huhehaote.aliyuncs.com",
+    "oss-cn-shenzhen": "oss-cn-shenzhen.aliyuncs.com",
+    "oss-cn-heyuan": "oss-cn-heyuan.aliyuncs.com",
+    "oss-cn-chengdu": "oss-cn-chengdu.aliyuncs.com",
+    "oss-cn-hongkong": "oss-cn-hongkong.aliyuncs.com",
+    "oss-us-west-1": "oss-us-west-1.aliyuncs.com",
+    "oss-us-east-1": "oss-us-east-1.aliyuncs.com",
+    "oss-ap-southeast-1": "oss-ap-southeast-1.aliyuncs.com",
+    "oss-ap-southeast-2": "oss-ap-southeast-2.aliyuncs.com",
+    "oss-ap-southeast-3": "oss-ap-southeast-3.aliyuncs.com",
+    "oss-ap-southeast-5": "oss-ap-southeast-5.aliyuncs.com",
+    "oss-ap-northeast-1": "oss-ap-northeast-1.aliyuncs.com",
+    "oss-ap-south-1": "oss-ap-south-1.aliyuncs.com",
+    "oss-eu-central-1": "oss-eu-central-1.aliyuncs.com",
+    "oss-eu-west-1": "oss-eu-west-1.aliyuncs.com",
+    "oss-me-east-1": "oss-me-east-1.aliyuncs.com",
+}
+
+
+def endpoint_to_region(endpoint):
+    for region in region_to_endpoint:
+        if endpoint == region_to_endpoint[region]:
+            return region
+    return None
+
+
+def load_credential_from_local():
+    config = configparser.ConfigParser()
+    config.read(os.path.join(os.path.expanduser("~"), ".ossutilconfig"))
+    return {
+        "accessKeyID": config["Credentials"]["accessKeyID"],
+        "accessKeySecret": config["Credentials"]["accessKeySecret"],
+        "endpoint": config["Credentials"]["endpoint"],
+        "region": endpoint_to_region(config["Credentials"]["endpoint"]),
+        "regionID": endpoint_to_region(config["Credentials"]["endpoint"])[4:],
+    }
+
+
+def encode(message):
+    return urllib.parse.quote(str(message), safe='', encoding="utf-8").replace("+", "%20").replace("*", "%2A").replace("%7E", "~")
+
+
+def signature(methods, params, access_key_secret):
+    kvs = "&".join([
+        "{}={}".format(encode(i), encode(params[i])) for i in sorted(params)
+    ])
+    to_sign = methods + "&" + encode("/") + "&" + "" + encode(kvs)
+    return base64.b64encode(hmac.new((access_key_secret + '&').encode(), to_sign.encode(), digestmod='sha1').digest()).decode()
+
+
+class TestUtil(unittest.TestCase):
+    def test_signature(self):
+        self.assertEqual(
+            signature("POST", {
+                "Project": "test-project",
+                "RegionId": "cn-shanghai",
+                "AccessKeyId": "testid",
+                "Format": "JSON",
+                "SignatureMethod": "HMAC-SHA1",
+                "SignatureVersion": "1.0",
+                "SignatureNonce": "d1ac7371108dc53541c9d0f29e5396c7",
+                "Timestamp": "2019-02-22T09:30:54Z",
+                "Action": "GetProject",
+                "Version": "2017-09-06",
+            }, "testsecret"),
+            "NPzJnV5HAdj4jkShTWKa9WwOZxU="
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
