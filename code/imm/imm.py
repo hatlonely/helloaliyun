2016fc3016ca4b89dc40f73eccc20d7e836f3840
hatlonely <hatlonely@gmail.com>
2020-03-26 20:41:00 +0800

    update

--- code/imm/imm.py
+++ code/imm/imm.py
@@ -8,18 +8,9 @@ import hmac
 import base64
 import os
 import configparser
+import util
 
-
-access_key_id = "see https://usercenter.console.aliyun.com/#/manage/ak"
-access_key_secret = "see https://usercenter.console.aliyun.com/#/manage/ak"
-
-def env():
-    global access_key_id
-    global access_key_secret
-    config = configparser.ConfigParser()
-    config.read(os.path.join(os.path.expanduser("~"), ".ossutilconfig"))
-    access_key_id = config["Credentials"]["accessKeyID"]
-    access_key_secret = config["Credentials"]["accessKeySecret"]
+access_key_id, access_key_secret = util.load_credential_from_local()
 
 # https://help.aliyun.com/document_detail/74947.html?spm=a2c4g.11186623.2.22.335226c5dUnarR
 def preview():
@@ -54,47 +45,16 @@ def convert():
         "TgtUri": "oss://hatlonely-test-bucket/hello.docx/imm"
     }
 
-    kvs = "&".join(["{}={}".format(encode(i), encode(params[i])) for i in sorted(params)])
-    to_sign = "POST" + "&" + encode("/") + "&" + "" + encode(kvs)
-    params["Signature"] = base64.b64encode(hmac.new((access_key_secret + '&').encode(), to_sign.encode(), digestmod='sha1').digest())
+    params["Signature"] = util.signature("POST", params, access_key_secret)
 
     res = requests.post("https://imm.cn-beijing.aliyuncs.com", params=params)
     print(res)
     print(res.content)
 
 
-def test_sign():
-    # http://imm.cn-shanghai.aliyuncs.com/?Project=test-project&RegionId=cn-shanghai&AccessKeyId=testid&Format=JSON&SignatureMethod=HMAC-SHA1&SignatureVersion=1.0&SignatureNonce=d1ac7371108dc53541c9d0f29e5396c7&Timestamp=2019-02-22T09%3A30%3A54Z&Action=GetProject&Version=2017-09-06
-    params = {
-        "Project": "test-project",
-        "RegionId": "cn-shanghai",
-        "AccessKeyId": "testid",
-        "Format": "JSON",
-        "SignatureMethod": "HMAC-SHA1",
-        "SignatureVersion": "1.0",
-        "SignatureNonce": "d1ac7371108dc53541c9d0f29e5396c7",
-        "Timestamp": "2019-02-22T09:30:54Z",
-        "Action": "GetProject",
-        "Version": "2017-09-06",
-    }
-
-    kvs = "&".join(["{}={}".format(encode(i), encode(params[i])) for i in sorted(params)])
-    to_sign = "POST" + "&" + encode("/") + "&" + "" + encode(kvs)
-    params["Signature"] = to_sign
-
-    print(to_sign)
-    print(base64.b64encode(hmac.new(b"testsecret&", to_sign.encode(), digestmod='sha1').digest()))
-
-    to_sign = "POST&%2F&AccessKeyId%3Dtestid&Action%3DGetProject&Format%3DJSON&Project%3Dtest-project&RegionId%3Dcn-shanghai&SignatureMethod%3DHMAC-SHA1&SignatureNonce%3Dd1ac7371108dc53541c9d0f29e5396c7&SignatureVersion%3D1.0&Timestamp%3D2019-02-22T09%253A30%253A54Z&Version%3D2017-09-06"
-    print(to_sign)
-    print(base64.b64encode(hmac.new(b"testsecret&", to_sign.encode(), digestmod='sha1').digest()))
-
-
 def main():
-    env()
-    # convert()
+    convert()
     preview()
-    # test_sign()
 
 if __name__ == "__main__":
     main()
---
+++ code/imm/util.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import os
+import configparser
+import urllib.parse
+import hmac
+import base64
+import unittest
+
+
+def load_credential_from_local():
+    config = configparser.ConfigParser()
+    config.read(os.path.join(os.path.expanduser("~"), ".ossutilconfig"))
+    access_key_id = config["Credentials"]["accessKeyID"]
+    access_key_secret = config["Credentials"]["accessKeySecret"]
+    return access_key_id, access_key_secret
+
+
+def encode(message):
+    return urllib.parse.quote(str(message), safe='', encoding="utf-8").replace("+", "%20").replace("*", "%2A").replace("%7E", "~")
+
+
+def signature(methods, params, access_key_secret):
+    kvs = "&".join(["{}={}".format(encode(i), encode(params[i])) for i in sorted(params)])
+    to_sign = methods + "&" + encode("/") + "&" + "" + encode(kvs)
+    return base64.b64encode(hmac.new((access_key_secret + '&').encode(), to_sign.encode(), digestmod='sha1').digest()).decode()
+
+
+class TestUtil(unittest.TestCase):
+    def test_signature(self):
+        self.assertEqual(
+            signature("POST", {
+                "Project": "test-project",
+                "RegionId": "cn-shanghai",
+                "AccessKeyId": "testid",
+                "Format": "JSON",
+                "SignatureMethod": "HMAC-SHA1",
+                "SignatureVersion": "1.0",
+                "SignatureNonce": "d1ac7371108dc53541c9d0f29e5396c7",
+                "Timestamp": "2019-02-22T09:30:54Z",
+                "Action": "GetProject",
+                "Version": "2017-09-06",
+            }, "testsecret"),
+            "NPzJnV5HAdj4jkShTWKa9WwOZxU="
+        )
+
+if __name__ == '__main__':
+    unittest.main()
